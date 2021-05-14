70e9d59f20390ba55148b20271696d607d40213c
kseniya <efremovaks@gmail.com>
2021-05-14 19:49:35 +0300

    шапка почти

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var navMain = document.querySelector('.main-nav');
+var navButton = document.querySelector('.main-nav__menu-button');
 
-pageHeader.classList.remove('page-header--nojs');
+navMain.classList.remove('main-nav--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navButton.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--open');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--open');
   }
 });
